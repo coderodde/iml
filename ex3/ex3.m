c9fc403466b05d4abbb122b83f28cc457498eb30
Rodion Efremov <rodion.efremov@helsinki.fi>
2014-11-10 15:36:11 +0200

    Coding task is done\!

--- ex3/ex3.m
+++ ex3/ex3.m
@@ -95,19 +95,34 @@ endfunction
 function predicted_label = knn_classify_impl(TrainingSet,
                                              TrainingSetY,
                                              image)
-    best_distance = Inf;
-    best_label = -1;
-    for i = 1:size(TrainingSet)(1)
-      current_dist = euclidean_distance(image, TrainingSet(i,:));
-      if best_distance > current_dist
-        best_distance = current_dist;
-        best_label = TrainingSetY(i);
-      endif
-    endfor
-    predicted_label = best_label;
+  % knn_classify_impl - predicts a digit for image 'image'.
+  %
+  % This function requires the actual training set 'TrainingSet' along the
+  % training labels 'TrainingSetY', and the image 'image' for which to
+  % predict the digit.
+  %
+  % Returns the predicted digit for 'image'.
+  best_distance = Inf;
+  best_label = -1;
+  for i = 1:size(TrainingSet)(1)
+    current_dist = euclidean_distance(image, TrainingSet(i,:));
+    if best_distance > current_dist
+      best_distance = current_dist;
+      best_label = TrainingSetY(i);
+    endif
+  endfor
+  predicted_label = best_label;
 endfunction
 
 function cm = knn_classify(TrainingSet, TrainingSetY, TestSet, TestSetY)
+  % knn_classify - classifies the images in 'TestSet' using
+  % training data in 'TrainingSet' and 'TrainingSetY', and produces the
+  % confusion matrix.
+  %
+  % This function requires the training data ('TrainingSet' and 'TrainingSetY')
+  % and the actual test data ('TestSet' and 'TestSetY').
+  %
+  % Returns the confusion matrix of kNN-classifier on give data.
   cm = zeros(10, 10);
   for i = 1:size(TestSet)(1)
     predicted_digit = knn_classify_impl(TrainingSet,
@@ -126,6 +141,13 @@ function cm = knn_classify(TrainingSet, TrainingSetY, TestSet, TestSetY)
 endfunction
 
 function err_rate = compute_error_rate(cm)
+  % compute_error_rate - computes the error rate given a confusion matrix.
+  %
+  % This function requires the actual confusion matrix with dimensions
+  % 10 x 10.
+  %
+  % Returns the error rate, which is the ratio of misses and the total amount
+  % of test.
   total = sum(cm(:));
   hits = 0;
   for i = 1:10
@@ -149,14 +171,14 @@ run('loadmnist.m');
 indices = randperm(5000, 100);
 
 # Display 100 chosen digits and print the respective classes.
-#visual(X(indices,:));
-#y(indices)
+visual(X(indices,:));
+y(indices)
 
 #### !!!! IMPORTANT !!!! #############################
 # On my Mac, gnuplot can display only one plot. If   #
 # your system does the same, close the current plot, #
 # and press Enter as instructed.                     #
-#input("Please close the gnuplot, and press Enter! ");#
+input("Please close the gnuplot, and press Enter! ");#
 ######################################################
 # Divide the data into the training set and the test set.
 TrainingSet = X(1:2500,:);
@@ -168,7 +190,7 @@ TestSetY = y(2501:5000);
 prototype_matrix = get_prototype_matrix(TrainingSet, TrainingSetY);
 
 # Plot them.
-#visual(prototype_matrix);
+visual(prototype_matrix);
 
 # Run the prototype-based classifier over the test set,
 # construct its confusion matrix, and print it along the error rate. 
@@ -176,5 +198,6 @@ cm1 = naive_classify(TestSet, TestSetY, prototype_matrix)
 printf("Error rate of prototype-based classifier: %f.\n",
        compute_error_rate(cm1));
        
+# Now, for the kNN-classifier.
 cm2 = knn_classify(TrainingSet, TrainingSetY, TestSet, TestSetY)
 printf("Error rate of kNN-classifier: %f.\n", compute_error_rate(cm2));
